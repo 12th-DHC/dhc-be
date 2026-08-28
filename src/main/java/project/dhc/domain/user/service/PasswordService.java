21d3909764dbfc9cb72bbad179f533fb246a10ed
unknown <a88763741@gmail.com>
2026-08-28 12:32:41 +0900

    fix(Exception): 익셉션 수정

--- src/main/java/project/dhc/domain/user/service/PasswordService.java
+++ src/main/java/project/dhc/domain/user/service/PasswordService.java
@@ -8,8 +8,8 @@ import project.dhc.domain.user.dto.request.PasswordChangeRequest;
 import project.dhc.domain.user.dto.response.PasswordChangeResponse;
 import project.dhc.domain.user.entity.Room;
 import project.dhc.domain.user.repository.RoomRepository;
-import project.dhc.global.exception.CustomException;
-import project.dhc.global.exception.ErrorCode;
+import project.dhc.global.exception.exceptions.InvalidPasswordException;
+import project.dhc.global.exception.exceptions.RoomNotFoundException;
 
 @Service // 비밀번호 변경 관련 비즈니스 로직
 @RequiredArgsConstructor // final 필드 생성자 자동 생성
@@ -28,7 +28,7 @@ public class PasswordService {
         // 방 번호로 방 조회
         Room room = roomRepository.findByRoomNumber(request.getRoomNumber())
                 .orElseThrow(() ->
-                        new CustomException(ErrorCode.ROOM_NOT_FOUND)
+                        RoomNotFoundException.EXCEPTION
                 );
 
         // 현재 비밀번호와 DB의 해시된 비밀번호 비교
@@ -36,7 +36,7 @@ public class PasswordService {
                 request.getCurrentPassword(),
                 room.getRoomPassword()
         )) {
-            throw new CustomException(ErrorCode.INVALID_PASSWORD);
+            throw InvalidPasswordException.EXCEPTION;
         }
 
         // 새 비밀번호를 BCrypt로 해싱
