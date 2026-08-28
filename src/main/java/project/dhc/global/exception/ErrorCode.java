b42f88890b97f93edd55921e8e41af155ee4c1cb
unknown <a88763741@gmail.com>
2026-08-28 22:34:58 +0900

    익셉션 에러 수정및 jwt비번 추가

--- src/main/java/project/dhc/global/exception/ErrorCode.java
+++ src/main/java/project/dhc/global/exception/ErrorCode.java
@@ -8,8 +8,12 @@ import lombok.Getter;
 public enum ErrorCode {
 	ERR_TEMPLATE(200, "ERR_TEMPLATE", "에러 템플릿입니다."),
 
+	NOT_VALID_DTO_ERR(400, "NOT_VALID_DTO_ERR", "유효하지 않은 요청입니다."),
+	VALIDATION_FAILED(400, "VALIDATION_FAILED", "요청 값이 올바르지 않습니다."),
+
 	INVALID_PASSWORD(401, "INVALID_PASSWORD", "비밀번호가 올바르지 않습니다.."),
 
+	NOT_FOUND(404, "NOT_FOUND", "리소스를 찾을 수 없습니다."),
 	ADMIN_NOT_FOUND(404, "ADMIN_NOT_FOUND", "관리자 계정이 존재하지 않습니다."),
 	ROOM_NOT_FOUND(404, "ROOM_NOT_FOUND", "존재하지 않는 호실입니다."),
 
