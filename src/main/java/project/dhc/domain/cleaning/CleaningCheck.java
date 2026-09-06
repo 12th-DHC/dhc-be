7d112470c14d50b57c2a3d439a9d370da7645550
unknown <a88763741@gmail.com>
2026-09-06 20:43:22 +0900

    스웨거 추가 및 머지 정리

--- src/main/java/project/dhc/domain/cleaning/CleaningCheck.java
+++ src/main/java/project/dhc/domain/cleaning/CleaningCheck.java
@@ -22,28 +22,28 @@ public class CleaningCheck {
     @Column(name = "date", nullable=false)
     private LocalDate date;
 
-    @Column(name = "A_passed", nullable = false)
+    @Column(name = "a_passed", nullable = false)
     private Boolean aPassed;
 
-    @Column(name = "A_notpass_reason")
+    @Column(name = "a_notpass_reason")
     private String aNotpassReason;
 
-    @Column(name = "A_ind_passed", nullable = false)
+    @Column(name = "a_ind_passed", nullable = false)
     private Boolean aIndPassed;
 
-    @Column(name = "A_ind_notpass_reason")
+    @Column(name = "a_ind_notpass_reason")
     private String aIndNotpassReason;
 
-    @Column(name = "B_passed", nullable = false)
+    @Column(name = "b_passed", nullable = false)
     private Boolean bPassed;
 
-    @Column(name = "B_notpass_reason")
+    @Column(name = "b_notpass_reason")
     private String bNotpassReason;
 
-    @Column(name = "B_ind_passed", nullable = false)
+    @Column(name = "b_ind_passed", nullable = false)
     private Boolean bIndPassed;
 
-    @Column(name = "B_ind_notpass_reason")
+    @Column(name = "b_ind_notpass_reason")
     private String bIndNotpassReason;
 
     @ManyToOne(fetch = FetchType.LAZY)
